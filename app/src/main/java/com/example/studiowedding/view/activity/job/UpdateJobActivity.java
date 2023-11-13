61084decec5c9ddc7c3c79c6ff7ce8b0ada6b161
Tys7603 <long.vlgt@gmail.com>
2023-11-14 00:22:04 +0700

    LongNX - Update job screen - 13/11/2023

---
+++ app/src/main/java/com/example/studiowedding/view/activity/job/UpdateJobActivity.java
@@ -0,0 +1,16 @@
+package com.example.studiowedding.view.activity.job;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.studiowedding.R;
+
+public class UpdateJobActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_job);
+    }
+}
