bf803efa469e645bd18c46c483f59a0b31f012f3
DELL <hominphuzxc@gmail.com>
2023-11-14 13:57:15 +0700

    PhuHm Config 14/11/2023

--- app/src/main/java/com/example/studiowedding/view/fragment/EmployeeFragment.java
+++ app/src/main/java/com/example/studiowedding/view/fragment/EmployeeFragment.java
@@ -1,4 +1,4 @@
-package com.example.studiowedding.view.fragment.notication;
+package com.example.studiowedding.view.fragment;
 
 import android.os.Bundle;
 
@@ -12,10 +12,10 @@ import com.example.studiowedding.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link NoticationFragment#newInstance} factory method to
+ * Use the {@link EmployeeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NoticationFragment extends Fragment {
+public class EmployeeFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class NoticationFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public NoticationFragment() {
+    public EmployeeFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class NoticationFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment NoticationFragment.
+     * @return A new instance of fragment EmployeeFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static NoticationFragment newInstance(String param1, String param2) {
-        NoticationFragment fragment = new NoticationFragment();
+    public static EmployeeFragment newInstance(String param1, String param2) {
+        EmployeeFragment fragment = new EmployeeFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class NoticationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notication, container, false);
+        return inflater.inflate(R.layout.fragment_employee, container, false);
     }
 }
