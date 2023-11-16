c5c052100a93a9dec94ac259ce88388da4dec227
anhntq <quocanh4567890@gmail.com>
2023-11-16 21:53:11 +0700

    AnhNTQ 16/11 man hinh search nhan vien

---
+++ app/src/main/java/com/example/studiowedding/view/activity/staff/SearchEmployeeActivity.java
@@ -0,0 +1,16 @@
+package com.example.studiowedding.view.activity.staff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.studiowedding.R;
+
+public class SearchEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_employee);
+    }
+}
