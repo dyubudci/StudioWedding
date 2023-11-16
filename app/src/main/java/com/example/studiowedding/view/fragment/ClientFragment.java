58f3ce51ca21778c39fef6ef69270a3615e4bdaf
CuongNeban <142796790+CuongNeban@users.noreply.github.com>
2023-11-16 18:34:58 +0700

    UI All

--- app/src/main/java/com/example/studiowedding/view/fragment/ClientFragment.java
+++ app/src/main/java/com/example/studiowedding/view/fragment/ClientFragment.java
@@ -3,64 +3,55 @@ package com.example.studiowedding.view.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.studiowedding.R;
+import com.example.studiowedding.adapter.ListClientAdapter;
+import com.example.studiowedding.model.ListclientModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ClientFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class ClientFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView recyclerViewCustomersList;
+    private RecyclerView.Adapter adapter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public ClientFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ClientFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ClientFragment newInstance(String param1, String param2) {
-        ClientFragment fragment = new ClientFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_client, container, false);
+        View view = inflater.inflate(R.layout.fragment_client, container, false);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerViewCustomersList = view.findViewById(R.id.RVCustomers);
+        recyclerViewCustomersList.setLayoutManager(linearLayoutManager);
+        ArrayList<ListclientModel> listCustomer = new ArrayList<>();
+        listCustomer.add(new ListclientModel("Cuong", "+ 888888888", "Đà nẵng"));
+        listCustomer.add(new ListclientModel("Cuong1", "+ 888888888", "Đà nẵng"));
+        listCustomer.add(new ListclientModel("Cuong2", "+ 888888888", "Đà nẵng"));
+        adapter = new ListClientAdapter(listCustomer);
+        recyclerViewCustomersList.setAdapter(adapter);
+
+        return view;
+
     }
 }
