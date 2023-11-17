35ae80503d85b022c2db4615e3184d8febd735b2
2023thong <thongpdpd06029@fpt.edu.vn>
2023-11-17 22:39:23 +0700

    ThongPD upload

--- app/src/main/java/com/example/studiowedding/view/activity/account/EditInformation.java
+++ app/src/main/java/com/example/studiowedding/view/activity/account/EditInformation.java
@@ -2,14 +2,24 @@ package com.example.studiowedding.view.activity.account;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 
 import com.example.studiowedding.R;
 
+import java.util.Calendar;
+
 public class EditInformation extends AppCompatActivity {
+
+    Calendar calendar = Calendar.getInstance();
+    int year = calendar.get(Calendar.YEAR);
+    int month = calendar.get(Calendar.MONTH);
+    int day = calendar.get(Calendar.DAY_OF_MONTH);
+
     ImageView imgback;
 
     @Override
@@ -23,5 +33,22 @@ public class EditInformation extends AppCompatActivity {
                 finish();
             }
         });
+
+
+//        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+//                new DatePickerDialog.OnDateSetListener() {
+//                    @Override
+//                    public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDay) {
+//                        // Xử lý ngày được chọn
+//                        String selectedDate = selectedDay + "/" + (selectedMonth + 1) + "/" + selectedYear;
+//                        // Hiển thị ngày đã chọn (bạn có thể làm điều gì đó khác với ngày này)
+//                        yourTextView.setText(selectedDate);
+//                    }
+//                }, year, month, day);
+//
+//        datePickerDialog.show();
+//
     }
+
+
 }
