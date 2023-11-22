2cc1e080b5f7bbe504d9fc254d97848c751ebd63
DELL <hominphuzxc@gmail.com>
2023-11-23 03:18:59 +0700

    PhuHM Done search product and service in screen select 23/11/2023

--- app/src/main/java/com/example/studiowedding/view/activity/detail_contract/ProductSelectActivity.java
+++ app/src/main/java/com/example/studiowedding/view/activity/detail_contract/ProductSelectActivity.java
@@ -7,7 +7,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.studiowedding.R;
@@ -17,6 +20,7 @@ import com.example.studiowedding.model.Service;
 import com.example.studiowedding.network.ApiClient;
 import com.example.studiowedding.network.ApiService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -27,8 +31,10 @@ public class ProductSelectActivity extends AppCompatActivity implements ProductS
     public static final String TAG = "ProductSelectActivity";
     public static final String PRODUCT_SELECTED_EXTRA = "ProductSelected";
     private ImageView backImageView;
+    private EditText searchEditText;
     private RecyclerView productSelectRecyclerView;
     private ProductSelectAdapter productSelectAdapter;
+    private List<Product> productList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,23 +44,30 @@ public class ProductSelectActivity extends AppCompatActivity implements ProductS
         initView();
         setListeners();
         setUpRecyclerView();
+        perfromCallAPIGetProducts();
 
-        ApiService apiService = ApiClient.getClient().create(ApiService.class);
-        Call<List<Product>> call = apiService.getProductsByStatusReady();
-        call.enqueue(new Callback<List<Product>>() {
+        // Tìm kiếm sản phẩm
+        searchEditText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
-                if (response.isSuccessful()) {
-                    List<Product> products = response.body();
-                    if (products != null) {
-                        productSelectAdapter.setProducts(products);
-                    }
-                }
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
 
             @Override
-            public void onFailure(Call<List<Product>> call, Throwable t) {
-                Log.e(TAG, "onFailure: " + t.toString());
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String searchTerm = charSequence.toString().toLowerCase();
+
+                List<Product> filteredProducts = new ArrayList<>();
+                for (Product product : productList) {
+                    if (product.getName().toLowerCase().contains(searchTerm)) {
+                        filteredProducts.add(product);
+                    }
+                }
+
+                productSelectAdapter.setProducts(filteredProducts);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
             }
         });
     }
@@ -62,6 +75,7 @@ public class ProductSelectActivity extends AppCompatActivity implements ProductS
     private void initView() {
         backImageView = findViewById(R.id.backImageView);
         productSelectRecyclerView = findViewById(R.id.productSelectRecyclerView);
+        searchEditText = findViewById(R.id.searchEditText);
     }
 
     private void setListeners() {
@@ -83,4 +97,28 @@ public class ProductSelectActivity extends AppCompatActivity implements ProductS
         setResult(Activity.RESULT_OK, intent);
         finish();
     }
+
+    // Thực hiện gọi api lấy danh sách sản phẩm
+    private void perfromCallAPIGetProducts() {
+        ApiService apiService = ApiClient.getClient().create(ApiService.class);
+        Call<List<Product>> call = apiService.getProductsByStatusReady();
+        call.enqueue(new Callback<List<Product>>() {
+            @Override
+            public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
+                if (response.isSuccessful()) {
+                    List<Product> products = response.body();
+                    if (products != null) {
+                        productSelectAdapter.setProducts(products);
+                        productList = products;
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Product>> call, Throwable t) {
+                Log.e(TAG, "onFailure: " + t.toString());
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/studiowedding/view/activity/detail_contract/ServiceSelectActivity.java
+++ app/src/main/java/com/example/studiowedding/view/activity/detail_contract/ServiceSelectActivity.java
@@ -7,15 +7,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.studiowedding.R;
 import com.example.studiowedding.adapter.ServiceSelectAdapter;
+import com.example.studiowedding.model.Product;
 import com.example.studiowedding.model.Service;
 import com.example.studiowedding.network.ApiClient;
 import com.example.studiowedding.network.ApiService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -28,6 +33,8 @@ public class ServiceSelectActivity extends AppCompatActivity implements ServiceS
     private ImageView backImageView;
     private RecyclerView serviceSelectRecyclerView;
     private ServiceSelectAdapter serviceSelectAdapter;
+    private List<Service> serviceList;
+    private EditText searchEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,35 +44,43 @@ public class ServiceSelectActivity extends AppCompatActivity implements ServiceS
         initView();
         setListeners();
         setUpdateRecyclerView();
-        ApiService apiService = ApiClient.getClient().create(ApiService.class);
-        Call<List<Service>> call = apiService.getSelectServices();
-        call.enqueue(new Callback<List<Service>>() {
+        performCallApiGetServices();
+
+        // Tìm kiếm dịch vụ
+        searchEditText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onResponse(Call<List<Service>> call, Response<List<Service>> response) {
-                if (response.isSuccessful()) {
-                    List<Service> services = response.body();
-                    if (services != null) {
-                        serviceSelectAdapter.setServices(services);
-                    }
-                }
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
 
             @Override
-            public void onFailure(Call<List<Service>> call, Throwable t) {
-                Log.e(TAG, "onFailure: " + t.toString());
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String searchTerm = charSequence.toString().toLowerCase();
+
+                List<Service> filteredServices = new ArrayList<>();
+                for (Service service : serviceList) {
+                    if (service.getName().toLowerCase().contains(searchTerm)) {
+                        filteredServices.add(service);
+                    }
+                }
+
+                serviceSelectAdapter.setServices(filteredServices);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
             }
         });
     }
+
 
     private void initView() {
         backImageView = findViewById(R.id.backImageView);
         serviceSelectRecyclerView = findViewById(R.id.serviceSelectRecyclerView);
+        searchEditText = findViewById(R.id.searchEditText);
     }
 
     private void setListeners() {
-        backImageView.setOnClickListener(view -> {
-            getOnBackPressedDispatcher().onBackPressed();
-        });
+        backImageView.setOnClickListener(view -> getOnBackPressedDispatcher().onBackPressed());
     }
 
     private void setUpdateRecyclerView() {
@@ -82,5 +97,28 @@ public class ServiceSelectActivity extends AppCompatActivity implements ServiceS
         intent.putExtra(SERVICE_SELECTED_EXTRA, service);
         setResult(Activity.RESULT_OK, intent);
         finish();
+    }
+
+    // Thực hiện gọi API lấy danh sách dịch vụ
+    private void performCallApiGetServices() {
+        ApiService apiService = ApiClient.getClient().create(ApiService.class);
+        Call<List<Service>> call = apiService.getSelectServices();
+        call.enqueue(new Callback<List<Service>>() {
+            @Override
+            public void onResponse(Call<List<Service>> call, Response<List<Service>> response) {
+                if (response.isSuccessful()) {
+                    List<Service> services = response.body();
+                    if (services != null) {
+                        serviceSelectAdapter.setServices(services);
+                        serviceList = services;
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Service>> call, Throwable t) {
+                Log.e(TAG, "onFailure: " + t.toString());
+            }
+        });
     }
 }
