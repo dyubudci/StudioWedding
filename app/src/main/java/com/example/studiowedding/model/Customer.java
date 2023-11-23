9e81e3a81f50159e7dff07e863af24838e309439
Đạt <datlowashere@gmail.com>
2023-11-23 09:11:42 +0700

    DatLT Update models 23/11/2023

--- app/src/main/java/com/example/studiowedding/model/Customer.java
+++ app/src/main/java/com/example/studiowedding/model/Customer.java
@@ -1,11 +1,30 @@
 package com.example.studiowedding.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.widget.ImageView;
 
-public class Customer {
+import androidx.annotation.NonNull;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class Customer implements Parcelable {
+    @SerializedName("idKhachHang")
+    private int id;
+    @SerializedName("tenKhachHang")
+
     private String name;
+    @SerializedName("dienThoai")
+
     private String phone;
+    @SerializedName("diaChi")
+
+
     private String address;
+
+
     private ImageView imgUpdateClient;
 
     public Customer(ImageView imgUpdateClient) {
@@ -18,6 +37,13 @@ public class Customer {
         this.address = address;
     }
 
+    public Customer(int id, String name, String phone, String address) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+    }
+
 
     public ImageView getImgUpdateClient() {
         return imgUpdateClient;
@@ -25,6 +51,10 @@ public class Customer {
 
     public void setImgUpdateClient(ImageView imgUpdateClient) {
         this.imgUpdateClient = imgUpdateClient;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -50,4 +80,36 @@ public class Customer {
     public void setAddress(String address) {
         this.address = address;
     }
+
+    protected Customer(Parcel in) {
+        id=in.readInt();
+        name = in.readString();
+        phone = in.readString();
+        address = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+        dest.writeString(phone);
+        dest.writeString(address);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Customer> CREATOR = new Creator<Customer>() {
+        @Override
+        public Customer createFromParcel(Parcel in) {
+            return new Customer(in);
+        }
+
+        @Override
+        public Customer[] newArray(int size) {
+            return new Customer[size];
+        }
+    };
 }
